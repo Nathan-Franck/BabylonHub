5d6ed182204e66a0654a6a7d7e8129349d3e68d6
Nathan Franck <nathanfranck1@gmail.com>
2023-06-29 12:27:19 -0600

    Load the Cleaning Marine rig 🤔

--- src/app.ts
+++ src/app.ts
@@ -1,35 +1,76 @@
-import { ArcRotateCamera, Engine, HemisphericLight, Mesh, MeshBuilder, Scene, Vector3 } from "babylonjs";
+import { ArcRotateCamera, Engine, HemisphericLight, Mesh, MeshBuilder, Scene, SceneLoader, Vector3 } from "babylonjs";
 import { Inspector } from "babylonjs-inspector";
 
-class App {
-  constructor() {
-    // create the canvas html element and attach it to the webpage
-    var canvas = document.createElement("canvas");
-    canvas.style.width = "100%";
-    canvas.style.height = "100%";
-    canvas.id = "gameCanvas";
-    document.body.style.margin = "0px";
-    document.body.style.width = "100%";
-    document.body.style.height = "100%";
-    document.body.style.overflow = "hidden";
-    var app = document.getElementById("app");
-    app.style.width = "100%";
-    app.style.height = "100%";
-    app.style.position = "absolute";
-    app.style.top = "0px";
-    app.style.left = "0px";
-    app.appendChild(canvas);
+async function run() {
+  // create the canvas html element and attach it to the webpage
+  var canvas = document.createElement("canvas");
+  canvas.style.width = "100%";
+  canvas.style.height = "100%";
+  canvas.id = "gameCanvas";
+  document.body.style.margin = "0px";
+  document.body.style.width = "100%";
+  document.body.style.height = "100%";
+  document.body.style.overflow = "hidden";
+  var app = document.getElementById("app");
+  app.style.width = "100%";
+  app.style.height = "100%";
+  app.style.position = "absolute";
+  app.style.top = "0px";
+  app.style.left = "0px";
+  app.appendChild(canvas);
 
-    // initialize babylon scene and engine
-    var engine = new Engine(canvas, true);
-    var scene = new Scene(engine);
+  // initialize babylon scene and engine
+  var engine = new Engine(canvas, true);
+  var scene = new Scene(engine);
 
-    var camera: ArcRotateCamera = new ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, Vector3.Zero(), scene);
-    camera.attachControl(canvas, true);
-    var light1: HemisphericLight = new HemisphericLight("light1", new Vector3(1, 1, 0), scene);
-    var sphere: Mesh = MeshBuilder.CreateSphere("sphere", { diameter: 1 }, scene);
+  var camera: ArcRotateCamera = new ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, Vector3.Zero(), scene);
+  camera.attachControl(canvas, true);
+  var light1: HemisphericLight = new HemisphericLight("light1", new Vector3(1, 1, 0), scene);
+  // var sphere: Mesh = MeshBuilder.CreateSphere("sphere", { diameter: 1 }, scene);
 
-    // hide/show the Inspector
+  // Load glb from public/FPS Rig.glb
+  var gltf = await SceneLoader.ImportMeshAsync(null, "/", "FPS Rig.glb", scene);
+  gltf.meshes[0].scaling = Vector3.One().scale(.1);
+
+  var armatureStructure = {
+    "Shoulder": {
+      "Elbow": {
+        "Forearm": {
+          "Hand": {
+            "Finger.Pinky.A": {
+              "Finger.Pinky.B": {
+                "Finger.Pinky.C": {}
+              }
+            },
+            "Finger.Ring.A": {
+              "Finger.Ring.B": {
+                "Finger.Ring.C": {}
+              }
+            },
+            "Finger.Middle.A": {
+              "Finger.Middle.B": {
+                "Finger.Middle.C": {}
+              }
+            },
+            "Finger.Index.A": {
+              "Finger.Index.B": {
+                "Finger.Index.C": {}
+              }
+            },
+            "Finger.Thumb.A": {
+              "Finger.Thumb.B": {
+                "Finger.Thumb.C": {}
+              }
+            }
+          }
+        }
+      }
+    }
+  };
+
+  console.log(gltf.transformNodes.find(t => t.name === "Shoulder"));
+
+    // Hide/show the Inspector
     window.addEventListener("keydown", (ev) => {
       // Shift+Ctrl+Alt+I
       if (ev.shiftKey && ev.ctrlKey && ev.altKey && ev.key === 'i') {
@@ -41,23 +82,24 @@ class App {
       }
     });
 
-    // setup inspector
-    Inspector.Show(scene, {
-      embedMode: true,
-      handleResize: true,
-      enableClose: false,
-      enablePopup: false,
-    });
-    // window resize handler
-    window.addEventListener("resize", () => {
-      engine.resize();
-    });
+  // Setup inspector
+  Inspector.Show(scene, {
+    embedMode: true,
+    handleResize: true,
+    enableClose: false,
+    enablePopup: false,
+  });
+
+  // Window resize handler
+  window.addEventListener("resize", () => {
+    engine.resize();
+  });
 
 
-    // run the main render loop
-    engine.runRenderLoop(() => {
-      scene.render();
-    });
-  }
+  // Run the main render loop
+  engine.runRenderLoop(() => {
+    scene.render();
+  });
 }
-new App();
+
+run();
